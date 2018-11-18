f2cc3d84f8f5a6c643ccbbc44bd4c1e7547450e8
pvrbek <petar.vrbek@gmail.com>
2018-11-18 03:08:15 +0100

    Fixed body to be responsive

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,8 +3,33 @@ import React, { Component } from "react";
 class Body extends React.Component{
 render(){
     return(
-        <div>
-            <h2>Body</h2>
+        <div className="body">
+            <div className="b-event--top">
+            </div>
+            <div className="b-event--middle">
+                <div className="b-event--middle_left">
+                    <div className="b-items_guests"></div>
+                    <div className="b-items_line"></div>
+                    <div className="b-items_publish"></div>
+                </div>
+                <div className="b-event--middle_right">
+                    <ul className="b-items_choice">
+                    <li className="b-items_choice--left -active"><a className='items_link--left items_link'>EVENT DETAILS</a></li>
+                    <li className="b-items_choice--right"><a className='items_link'>FIND A TIME</a></li>
+                    </ul>
+                    <div className="b-items_hline"></div>
+                    <div className="b-items-details">
+                        <div className="b-items-details_inputs"></div>
+                        <div className="b-items-details_vline"></div>
+                        <div className="b-items-details_drops">
+                            <div className="b-items_drops--top"></div>
+                            <div className="b-items_drops--middle"></div>
+                            <div className="b-items_drops-bottom"></div>
+                        </div>
+                    </div>
+                    <div className="b-items_hline--bottom"></div>
+                </div>
+            </div>
         </div>
 
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,17 +3,14 @@ import React, { Component } from "react";
 class Header extends React.Component{
 render(){
     return(
-        <div>
-            <div>
-                <h1>header-container-first</h1>
+        <header className="header">
+            <div className='h-event--first'>
             </div>
-            <div>
-                <h1>header-container</h1>
+            <div className='h-event'>
             </div>
-            <div>
-                <h1>header-container-last</h1>
+            <div className='h-event--last'>
             </div>
-        </div>
+        </header>
 
     );
 
