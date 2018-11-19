b3c5a42519f1c2159d82b35cbebb7b1ed80aed74
pvrbek <petar.vrbek@gmail.com>
2018-11-19 01:35:56 +0100

    Sredjen body header i dodana forma za dodavanje gostiju

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,20 +5,45 @@ render(){
     return(
         <div className="body">
             <div className="b-event--top">
+                <div className="b-event--top_left">
+                    <div className='b-head_search'>
+                        <input id='b-head_search--name' type='text' defaultValue='Dribbble Meetup, Zagre' name='eventName'/>
+                        <button id='b-head_search--save'>SAVE EVENT</button>
+                    </div>
+                </div>
+                <div className="b-event--top_right">
+                    <div className='b-head_clocks'>
+                        <div className='b-head_clocks--left'>
+                            <div className='b-head_clocks--left_top'>5:00pm</div>
+                            <div className='b-head_clocks--left_bottom'>11/25/2016</div>
+                        </div>
+                        <div className='b-head_clocks--right'>
+                            <div className='b-head_clocks--right_top'>6:00pm</div>
+                            <div className='b-head_clocks--right_bottom'>11/25/2016</div>
+                        </div>
+                        <div className='b-head_clocks--zone'>
+                            <a>Time zone</a>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div className="b-event--middle">
                 <div className="b-event--middle_left">
-                    <div className="b-items_guests"></div>
-                    <div className="b-items_line"></div>
-                    <div className="b-items_publish"></div>
+                    <div className="b-items_guests">
+                        <h2>Add Guests</h2>
+                    
+                    <form className='b-items_guests--form'>
+
+                    </form>
+                    </div>
                 </div>
                 <div className="b-event--middle_right">
                     <ul className="b-items_choice">
                     <li className="b-items_choice--left -active"><a className='items_link--left items_link'>EVENT DETAILS</a></li>
                     <li className="b-items_choice--right"><a className='items_link'>FIND A TIME</a></li>
                     </ul>
-                    <div className="b-items_hline"></div>
                     <div className="b-items-details">
+                        <div className="b-items-details_hline"></div>
                         <div className="b-items-details_inputs"></div>
                         <div className="b-items-details_vline"></div>
                         <div className="b-items-details_drops">
@@ -26,8 +51,8 @@ render(){
                             <div className="b-items_drops--middle"></div>
                             <div className="b-items_drops-bottom"></div>
                         </div>
+                        <div className="b-items-details_hline--bottom"></div>
                     </div>
-                    <div className="b-items_hline--bottom"></div>
                 </div>
             </div>
         </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,43 @@ render(){
     return(
         <header className="header">
             <div className='h-event--first'>
+                <img className="h-img-back" src="./src/images/ic-navigate-before-black-24-dp-1-x.png" alt="Navigate-Before"/>
             </div>
-            <div className='h-event'>
+            <div className='h-event--middle'>
+                    <div className='h-button-container'>
+                        <div className='h-save-container'>
+                            <button id='h-save-button'>SAVE</button>
+                        </div>
+                        <div className='h-discard-container'>
+                            <button id='h-discard-button'>
+                            <img className="h-img-discard" src="./src/images/ic-cancel-white-24-dp-1-x.png" alt="Cancel"></img>
+                                <p>DISCARD</p>
+                            </button>
+                        </div>
+                        <div className='h-delete-container'>
+                            <button id='h-delete-button'>
+                            <img className="h-img-delete" src="./src/images/ic-delete-white-24-dp-1-x.png" alt="Delete"></img>
+                                <p>DELETE</p>
+                            </button>
+                        </div>
+                    </div>
             </div>
             <div className='h-event--last'>
+                <div className='h-event--last_notification'>
+                    <img className='h-img-notif' src="./src/images/ic-notifications-none-black-24-dp-1-x.png" alt="Notification"></img>
+                </div>
+                <div className='h-event--last_menu'>
+                    <img className='h-img-menu' src="./src/images/ic-apps-black-24-dp.png" alt="Menu"></img>
+                </div>
+            </div>
+            <div className='h-event--arrow'>
+                <img className='h-img-arrow' src="./src/images/ic-navigate-before-black-24-dp-1-x.png" alt="Menu"></img>
+            </div>
+            <div className='h-event--profile'>
+                <img className='h-img-profile' src="./src/images/ic-navigate-before-black-24-dp-1-x.png" alt="Menu"></img>
+            </div>
+            <div className='h-event--search'>
+                <img className='h-img-search' src="./src/images/ic-navigate-before-black-24-dp-1-x.png" alt="Menu"></img>
             </div>
         </header>
 
